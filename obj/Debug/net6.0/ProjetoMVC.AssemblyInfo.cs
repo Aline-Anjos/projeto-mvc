c02fe8860b12c7854af0558864d1c29d55f26d50
Aline Anjos <aline.aanjosx@gmail.com>
2024-02-26 23:38:45 -0300

    Configurando o Entity Framework

--- obj/Debug/net6.0/ProjetoMVC.AssemblyInfo.cs
+++ obj/Debug/net6.0/ProjetoMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e198bbdc635e972e0d656d587bce27fd28140f80")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
