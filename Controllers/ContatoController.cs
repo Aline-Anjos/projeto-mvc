54f8870b1ed0398b5d964e8f5d54ce51e07da96d
Aline Anjos <aline.aanjosx@gmail.com>
2024-02-29 11:22:51 -0300

    Configurando o metodo na controller

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoMVC.Context;
 
 namespace ProjetoMVC.Controllers
 {
     public class ContatoController : Controller
     {
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var contatos = _context.Contatos.ToList();
+            return View(contatos);
         }
     }
 }
